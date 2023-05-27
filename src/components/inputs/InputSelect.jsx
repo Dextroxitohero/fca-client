431c89860afdec10ffd914c0f94cb6ee7ccb29fc
Eric Hernandez <erichernandez@MacBook-Pro-de-Eric.local>
2023-05-26 18:47:18 -0600

    migration input component and replace form react by formik

--- src/components/inputs/InputSelect.jsx
+++ src/components/inputs/InputSelect.jsx
@@ -1,31 +1,26 @@
-export const Input = ({
+export const InputSelect = ({
     id,
     name,
     label,
-    type = "text",
     placeholder,
     disabled,
     formik,
+    data,
+    optionDefault,
     error
 }) => {
+
     return (
         <div>
             <label
-                htmlFor={name}
-                className="
-                    block 
-                    text-sm 
-                    font-medium 
-                    leading-6 
-                    text-gray-900"
-            >
+                htmlFor="country"
+                className="block text-sm font-medium leading-6 text-gray-900">
                 {label}
             </label>
             <div className="mt-2">
-                <input
+                <select
                     id={id}
                     name={name}
-                    type={type}
                     onChange={formik.handleChange}
                     onBlur={formik.handleBlur}
                     value={formik.values.name}
@@ -37,24 +32,37 @@ export const Input = ({
                         rounded-md 
                         border-0 
                         py-1.5 
-                        px-2
                         text-gray-900 
                         shadow-sm 
                         ring-1 
                         ring-inset 
                         ring-gray-300 
-                        placeholder:text-gray-400 
                         focus:ring-2 
                         focus:ring-inset 
                         focus:ring-indigo-600 
                         sm:text-sm 
                         sm:leading-6
                         ${error ? 'ring-rose-500' : 'ring-gray-300'}
-                        ${error ? 'focus:ring-rose-500'  : 'focus:border-neutral-black'}
-                    `}
-                />
-                {error && <div className="text-red-500">{error}</div>}
+                        ${error ? 'focus:ring-rose-500' : 'focus:border-neutral-black'}
+                        `
+                    }
+                >
+                    <option value="">{optionDefault}</option>
+                    {
+                        data.map(item => (
+                            <option
+                                key={item.value}
+                                value={item.value}
+                                placeholder={placeholder}
+                            >
+                                {item.value}
+                            </option>
+                        ))
+
+                    }
+                </select>
             </div>
+            {error && <div className="text-red-500">{error}</div>}
         </div>
     )
 }
