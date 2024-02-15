c02b49761a496914f803d1157f4dd038dc9d85e2
Eric Hernandez <ejhernandez@apexsystems.com>
2024-02-15 02:27:22 -0600

    updated url server

--- src/common/urlBase.js
+++ src/common/urlBase.js
@@ -1,4 +1,4 @@
-    export const baseURLImage = 'http://localhost:8000/uploads/images/';
-    // export const baseURLImage = 'https://fca-server-production.up.railway.app/uploads/images/';
+    // export const baseURLImage = 'http://localhost:8000/uploads/images/';
+    export const baseURLImage = 'https://fca-server-production.up.railway.app/uploads/images/';
 
     export const urlFlag = `https://fca-server-production.up.railway.app/uploads/flags/`;
